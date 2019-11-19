d8a6f70c3ab2549e02bdf4d321498a512423f121
bcbcbcbcbcbc <57931812+bcbcbcbcbcbc@users.noreply.github.com>
2019-11-19 17:57:15 +0900

    Add files via upload
    First Commit

---
+++ token/src/main/java/com/text/token/TextTokenizerApplication.java
@@ -0,0 +1,13 @@
+package com.text.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextTokenizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextTokenizerApplication.class, args);
+	}
+
+}
---
+++ token/src/test/java/com/text/token/TextTokenizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.text.token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextTokenizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
